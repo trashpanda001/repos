1347f3211db1881d6f6087fecc71c61ad2bd240c
Trash Panda <hi@trashpanda.com>
2024-10-09 20:45:09 -0700

    Added sentry-javascript

--- update.mjs
+++ update.mjs
@@ -5,6 +5,7 @@ const repos = [
   "git@github.com:elixir-lang/elixir",
   "git@github.com:facebook/lexical",
   "git@github.com:framer/motion",
+  "git@github.com:getsentry/sentry-javascript",
   "git@github.com:mui/material-ui",
   "git@github.com:pmndrs/jotai",
   "git@github.com:shadcn-ui/ui",
